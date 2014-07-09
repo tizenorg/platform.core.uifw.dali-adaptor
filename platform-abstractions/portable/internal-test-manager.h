f5c4e469035c52ef83c999eaa412320822341f70
Andrew Cox <andrew.cox@partner.samsung.com>
2014-07-09 20:39:29 +0100

    A simple testing framework for in-situ testing of code not visible from outside the compilation unit
    [problem]
    Impossible to directly test code that is not visible outside its compilation unit.
    
    [cause]
    Design of testing framework.
    
    [solution]
    This patch is intended to allow the testing of code which is not visible
    outside its compilation unit such as small utility functions in unnamed
    namespaces.
    
    Usage:
    Simply include platform-abstractions/portable/internal-test.h and place
    a block of code like the one below outside function or class scope in
    any namespace of any .cpp file within the library. This example tests
    a hypothetical user-written int mul(int, int) function.
    
    DALI_BEGIN_INTERNAL_TEST_NO_SUITE( MyTestName )
    
    const int mulled = mul( mul ( 2, 3 ), mul( 5, 7 ) );
    const int simpleMulled = 2 * 3 * 5 * 7;
    DALI_INTERNAL_TEST_CHECK( mulled == simpleMulled );
    
    DALI_END_INTERNAL_TEST( MyTestName )
    
    See README.md for more details.
    
    Change-Id: Idc744907e070698d0940a7fe47182dcd6e7bf022
    Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

---
+++ platform-abstractions/portable/internal-test-manager.h
@@ -0,0 +1,78 @@
+#ifndef _DALI_INTERNAL_PLATFORM_INTERNAL_TEST_MANAGER_H_
+#define DALI_INTERNAL_PLATFORM_INTERNAL_TEST_MANAGER_H_
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifdef DALI_INTERNAL_TESTS_ENABLED
+
+// INTERNAL INCLUDES
+
+// EXTERNAL INCLUDES
+#include <string>
+#include <dali/public-api/common/map-wrapper.h>
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Platform
+{
+
+class InternalTest;
+
+/** @brief Map from test name to test instance. */
+typedef std::map< std::string, InternalTest* > Suite;
+
+/** @brief Map from suite name to a suite's collection of tests. */
+typedef std::map< std::string, Suite > SuiteCollection;
+
+class InternalTestManager
+{
+public:
+  enum TestResult
+  {
+    TestResultSuccess = 0,
+    TestResultFailure = 1
+  };
+private:
+
+  InternalTestManager();
+  ~InternalTestManager();
+
+public:
+  void RegisterTest( InternalTest& test, std::string testName , std::string suiteName );
+  TestResult RunTest( std::string test, std::string suite );
+  TestResult RunSuite( std::string suite );
+  TestResult RunAll();
+
+  static InternalTestManager& GetInstance();
+
+private:
+  // data:
+  SuiteCollection mSuites;
+};
+
+inline InternalTestManager::TestResult UpdateTestResult( InternalTestManager::TestResult oldTestResult, InternalTestManager::TestResult newTestResult )
+{
+  return oldTestResult == InternalTestManager::TestResultFailure ? InternalTestManager::TestResultFailure : newTestResult;
+}
+
+} /* namespace Platform */
+} /* namespace Internal */
+} /* namespace Dali */
+
+#endif // DALI_INTERNAL_TESTS_ENABLED
+#endif /* DALI_INTERNAL_PLATFORM_INTERNAL_TEST_MANAGER_H_ */
