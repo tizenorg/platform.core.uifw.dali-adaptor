04e389a151a0036af3c75d1261550fe51038b154
Adeel Kazmi <adeel.kazmi@samsung.com>
2014-09-01 18:45:12 +0100

    Added SingletonService & removed Singleton APIs from Adaptor
    Change-Id: I09191b12dfb40c54e68f3bfa811bd61b70248780

--- adaptors/common/application-impl.h
+++ adaptors/common/application-impl.h
@@ -28,14 +28,15 @@
 
 // INTERNAL INCLUDES
 #include <application.h>
+#include <singleton-service.h>
 
 #include <framework.h>
 #include <window-impl.h>
 
 namespace Dali
 {
-class Window;
 class Adaptor;
+class Window;
 
 namespace Internal
 {
@@ -72,7 +73,7 @@ public:
                             const DeviceLayout& baseLayout,
                             Dali::Application::WINDOW_MODE windowMode);
 
-  Application(int* argc, char **argv[], const std::string& name, const DeviceLayout& baseLayout, Dali::Application::WINDOW_MODE windowMode );
+  Application( int* argc, char **argv[], const std::string& name, const DeviceLayout& baseLayout, Dali::Application::WINDOW_MODE windowMode );
 
   /**
    * Destructor
@@ -115,6 +116,13 @@ public:
    * @copydoc Dali::Application::Get();
    */
   static Dali::Application Get();
+
+  /**
+   * @brief Checks if the Application is available.
+   *
+   * @return true if available, false otherwise
+   */
+  static bool IsAvailable();
 
   /**
    * @copydoc Dali::Application::GetTheme();
@@ -262,6 +270,7 @@ private:
 
   CommandLineOptions*                   mCommandLineOptions;
 
+  Dali::SingletonService                mSingletonService;
   Dali::Adaptor*                        mAdaptor;
   Dali::Window                          mWindow;
   Dali::Application::WINDOW_MODE        mWindowMode;
