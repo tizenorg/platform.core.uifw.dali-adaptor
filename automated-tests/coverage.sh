86f6653a0ec9fc995d8cca5d1a2e645d503b4c36
Adeel Kazmi <adeel.kazmi@samsung.com>
2014-09-12 18:16:21 +0100

    (Build) Ensure branch coverage is collected for later versions of lcov
    Change-Id: I52200982606e14c2191470fd8bc3b60a776e9271

--- automated-tests/coverage.sh
+++ automated-tests/coverage.sh
@@ -2,6 +2,7 @@
 
 ( cd ../build/tizen ; make cov_data )
 
+LCOV_OPTS=`echo | awk -v version=\`printf "\\\`lcov --version | sed 's/^.* //g'\\\`\n1.10\n" | sort -V | head -n 1\` '{if ( version == 1.10 ) printf("--rc lcov_branch_coverage=1\n", version);}'`
 
 for i in `find . -name "*.dir"` ; do
     (
@@ -10,15 +11,15 @@ for i in `find . -name "*.dir"` ; do
         covs=( `ls *.gcda 2>/dev/null` )
         if [[ $? -eq 0 ]]
         then
-            lcov --directory . -c -o dali.info
-            lcov --remove dali.info "*boost*" "/usr/include/*" "*/automated-tests/*" -o dali.info
+            lcov $LCOV_OPTS --directory . -c -o dali.info
+            lcov $LCOV_OPTS --remove dali.info "*boost*" "/usr/include/*" "*/automated-tests/*" -o dali.info
         fi
     )
 done
 
 (
     cd .. ;
-    genhtml -o build/tizen/doc/coverage `find . -name dali.info`
+    genhtml $LCOV_OPTS -o build/tizen/doc/coverage `find . -name dali.info`
 )
 
 echo "Coverage output: ../build/tizen/doc/coverage/index.html"
