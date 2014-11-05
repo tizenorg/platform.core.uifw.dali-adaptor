df752fc2e6f57bad705d99dc68854ed6ddfd395e
Xiangyin Ma <x1.ma@samsung.com>
2014-11-05 19:25:21 +0000

    Adaptor: Fix Klocwork issues
    [Problem] Klocwork issues
    
    [Cause] 1. Data member not initialized in the constructor or non-used data member
            2. Floating point expression is test for equality
            3. Constant condition in conditional expression
            4. Memory leak
            5. syntax shortcuts: variable used twice in one expression where one usage is subject to side-effects
            6. Unreachable code
    
    [Solution]
    
    Change-Id: I313aabfc9db262ded0406fb3621f8914bac2c1ec

--- platform-abstractions/slp/resource-loader/resource-loader.cpp
+++ platform-abstractions/slp/resource-loader/resource-loader.cpp
@@ -677,7 +677,12 @@ GlyphSet* ResourceLoader::GetCachedGlyphData(const TextResourceType& textRequest
 
       // create a new bitmap, and copy in the data
       BitmapPtr bitmapData ( Integration::Bitmap::New(Bitmap::BITMAP_2D_PACKED_PIXELS, ResourcePolicy::DISCARD) );
-      DALI_ASSERT_ALWAYS( data.length == DISTANCE_FIELD_SIZE * DISTANCE_FIELD_SIZE );
+      if( data.length != DISTANCE_FIELD_SIZE * DISTANCE_FIELD_SIZE)
+      {
+        delete glyphSet;
+        delete dataCache;
+        DALI_ASSERT_ALWAYS(false && "Length of cached glyph data is unequal to distance field size");
+      }
 
       // assign the data
       bitmapData->GetPackedPixelsProfile()->AssignBuffer( Pixel::A8, data.data, DISTANCE_FIELD_SIZE * DISTANCE_FIELD_SIZE, DISTANCE_FIELD_SIZE, DISTANCE_FIELD_SIZE );
@@ -859,6 +864,7 @@ Integration::BitmapPtr ResourceLoader::GetGlyphImage( FT_Library freeType, const
   if( NULL != slpFace )
   {
     image = GetGlyphBitmap( slpFace->face, character );
+    delete slpFace;
   }
 
   return image;
