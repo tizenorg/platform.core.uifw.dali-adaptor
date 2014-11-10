077fde50d78db207db11adadbe3e74968f25efde
Kingsley Stephens <k.stephens@partner.samsung.com>
2014-11-10 17:24:58 +0000

    Expose custom performance timers
    Change-Id: I4e11fc2a4d82a6a2b5ea3366067ff033a76ff219

--- adaptors/public-api/dali.h
+++ adaptors/public-api/dali.h
@@ -41,6 +41,7 @@
 #include <dali/public-api/adaptor-framework/key.h>
 #include <dali/public-api/adaptor-framework/orientation.h>
 #include <dali/public-api/adaptor-framework/physical-keyboard.h>
+#include <dali/public-api/adaptor-framework/performance-logger.h>
 #include <dali/public-api/adaptor-framework/pixmap-image.h>
 #include <dali/public-api/adaptor-framework/render-surface.h>
 #include <dali/public-api/adaptor-framework/singleton-service.h>
