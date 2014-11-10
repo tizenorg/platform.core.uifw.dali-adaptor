e9a374be234b5a29532d548d66835ddcd169e798
Kingsley Stephens <k.stephens@partner.samsung.com>
2014-11-10 17:24:58 +0000

    Expose custom performance timers
    Change-Id: I4e11fc2a4d82a6a2b5ea3366067ff033a76ff219

--- adaptors/base/performance-logging/performance-server.h
+++ adaptors/base/performance-logging/performance-server.h
@@ -22,6 +22,7 @@
 #include <base/performance-logging/frame-time-stats.h>
 #include <dali/public-api/common/dali-vector.h>
 #include <base/interfaces/adaptor-internal-services.h>
+#include <base/performance-logging/performance-marker.h>
 
 // EXTERNAL INCLUDES
 #include <boost/thread/mutex.hpp>
@@ -36,6 +37,7 @@ namespace Adaptor
 {
 
 class EnvironmentOptions;
+
 /**
  * Concrete implementation of performance interface.
  * Adaptor classes should never include this file, they
@@ -43,7 +45,6 @@ class EnvironmentOptions;
  */
 class PerformanceServer : public PerformanceInterface
 {
-
 public:
 
   /**
@@ -60,22 +61,76 @@ public:
   virtual ~PerformanceServer();
 
   /**
+   * @copydoc PerformanceLogger::AddContext()
+   */
+  virtual ContextId AddContext( const char* name );
+
+  /**
+   * @copydoc PerformanceLogger::RemoveContext()
+   */
+  virtual void RemoveContext( ContextId contextId );
+
+  /**
    * @copydoc PerformanceInterface::AddMarker()
    */
-  virtual void AddMarker( PerformanceMarker::MarkerType markerType);
+  virtual void AddMarker( MarkerType markerType );
+
+  /**
+   * @copydoc PerformanceLogger::AddMarker()
+   */
+  virtual void AddMarker( MarkerType markerType, ContextId contextId );
 
   /**
    * @copydoc PerformanceInterface::SetLogging()
    */
-  virtual void SetLogging( unsigned int level, unsigned int interval);
+  virtual void SetLogging( unsigned int level, unsigned int logFrequency );
 
+  /**
+   * @copydoc PerformanceLogger::SetLoggingFrequency()
+   */
+  virtual void SetLoggingFrequency( unsigned int logFrequency, ContextId contextId );
+
+  /**
+   * @copydoc PerformanceLogger::EnableLogging()
+   */
+  virtual void EnableLogging( bool enable, ContextId contextId );
 
 private:
 
   /**
+   * Struct to hold a stat context
+   */
+  struct StatContext
+  {
+    typedef Dali::Vector< PerformanceMarker > MarkerVector;
+
+    /**
+     * Default constructor
+     */
+    StatContext();
+
+    /**
+     * Constructor
+     *
+     * @param[in] contextName Name of the context to print in console
+     */
+    StatContext( const char* contextName );
+
+    const char* name;           ///< Name of the context
+    FrameTimeStats stats;       ///< Frame time stats to accumulate
+
+    MarkerVector markers;      ///< vector of markers
+    boost::mutex dataMutex;    ///< mutex
+
+    unsigned int logFrequencyMicroseconds; ///< if logging is enabled, what frequency to log out at in micro-seconds
+
+    bool log;                  ///< Whether to print the log for this context or not
+  };
+
+  /**
    * Helper
    */
-  void AddMarkerToLog( PerformanceMarker marker );
+  void AddMarkerToLog( const PerformanceMarker& marker, ContextId contextId );
 
   /**
    * Helper
@@ -85,28 +140,61 @@ private:
   /**
    * log markers out
    */
-  void LogMarkers();
+  void LogMarkers( StatContext* context );
+
+  /**
+   * Retrieve a stat context by ID
+   *
+   * @param[in] contextId The ID of the context to retrieve
+   * @return Return the stat context if it exists or null
+   */
+  StatContext* GetContext( ContextId contextId );
+
+  /**
+   * Return a string representation of the marker type
+   *
+   * @param[in] type The marker type
+   * @return Return the name of the given marker
+   */
+  const char* GetMarkerName( MarkerType type ) const;
 
 private:
 
-  bool mLoggingEnabled:1;         ///< whether logging update / render to a log is enabled
-  bool mLogFunctionInstalled:1;   ///< whether the log function is installed
-  unsigned int mLogLevel;         ///< log level
+  struct StatContextIdPair
+  {
+    /**
+     * Default constructor
+     */
+    StatContextIdPair() {}
 
-  unsigned int mLogFrequencyMicroseconds; ///< if logging is enabled, what frequency to log out at in micro-seconds
+    /**
+     * Constructor
+     */
+    StatContextIdPair( StatContext* statContext, unsigned int contextId ) : context( statContext ), id( contextId ) {}
 
-  FrameTimeStats mUpdateStats;    ///< update time statistics
-  FrameTimeStats mRenderStats;    ///< render time statistics
-  FrameTimeStats mEventStats;     ///< event time statistics
+    StatContext* context;   ///< A raw pointer to a StateContext
+    unsigned int id;        ///< The ID of the context
+  };
+
+  typedef Dali::Vector< StatContextIdPair > StatContexts;    // Using a map as contexts may be added/deleted at any time
+
+  // The list of stat contexts
+  StatContexts mStatContexts;
 
   Integration::PlatformAbstraction& mPlatformAbstraction; ///< platform abstraction
+  const EnvironmentOptions& mEnvironmentOptions;          ///< environment options
+  KernelTraceInterface& mKernelTrace;                     ///< kernel trace interface
 
-  typedef Dali::Vector<PerformanceMarker > MarkerVector;
-  MarkerVector mMarkers;              ///< vector of markers
-  boost::mutex mDataMutex;            ///< mutex
-  const EnvironmentOptions& mEnvironmentOptions;      ///< environment options
-  KernelTraceInterface& mKernelTrace; ///< kernel trace interface
+  unsigned int mLogLevel;         ///< log level
+  unsigned int mNextContextId;    ///< The next valid context ID
 
+  // Some defaults
+  ContextId mUpdateStats;    ///< update time statistics
+  ContextId mRenderStats;    ///< render time statistics
+  ContextId mEventStats;     ///< event time statistics
+
+  bool mLoggingEnabled:1;         ///< whether logging update / render to a log is enabled
+  bool mLogFunctionInstalled:1;   ///< whether the log function is installed
 };
 
 
