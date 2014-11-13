13d31aa282eda3bb74043a7d3aac13a0841411fc
Andrew Cox <andrew.cox@partner.samsung.com>
2014-11-13 17:40:31 +0000

    Tests for Image Loading Scaling Modes
    [problem]
    We have multiple modes for scaling images at load time
    but no tests that systematically use those modes.
    
    [solution]
    Set of tests which load images of various aspect ratios and file types
    using all of our ImageAttributes::ScalingModesSet of tests which load
    images of various aspect ratios and file types using all of our
    ImageAttributes::ScalingModes.
    
    Change-Id: I3fe5ea1b1cc608f7971f527a4f46e268cb0fc3a9
    Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

--- automated-tests/src/dali-platform-abstraction/resource-collector.cpp
+++ automated-tests/src/dali-platform-abstraction/resource-collector.cpp
@@ -26,8 +26,9 @@ namespace Platform
 {
 using namespace Dali::Integration;
 
-ResourceCollector::ResourceCollector() :
-  mGrandTotalCompletions(0)
+ResourceCollector::ResourceCollector( LoadCompletionMonitor * const completionMonitor ) :
+  mLoadCompletionMonitor( completionMonitor ),
+  mGrandTotalCompletions( 0 )
 {
 }
 
@@ -35,14 +36,20 @@ ResourceCollector::~ResourceCollector() {}
 
 void ResourceCollector::LoadResponse( Dali::Integration::ResourceId id, Dali::Integration::ResourceTypeId type, Dali::Integration::ResourcePointer resource, Dali::Integration::LoadStatus status )
 {
+
   if( status == RESOURCE_COMPLETELY_LOADED )
   {
-    DALI_ASSERT_DEBUG( mCompletionCounts.find(id) == mCompletionCounts.end() && "A resource can only complete once." );
     mCompletionStatuses[id] = true;
+    DALI_ASSERT_DEBUG( mCompletionCounts.find(id) == mCompletionCounts.end() && "A resource can only complete once." );
     ++mCompletionCounts[id];
     ++mSuccessCounts[id];
     mCompletionSequence.push_back( id );
     ++mGrandTotalCompletions;
+
+    if( mLoadCompletionMonitor )
+    {
+       mLoadCompletionMonitor->LoadSucceeded( *this, id, type, resource );
+    }
   }
 }
 
@@ -52,6 +59,11 @@ void ResourceCollector::LoadFailed( Dali::Integration::ResourceId id, Dali::Inte
   ++mFailureCounts[id];
   mCompletionSequence.push_back( id );
   ++mGrandTotalCompletions;
+
+  if( mLoadCompletionMonitor )
+  {
+     mLoadCompletionMonitor->LoadFailed( *this, id, failure );
+  }
 }
 
 
