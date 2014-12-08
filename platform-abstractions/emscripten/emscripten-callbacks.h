24f5fbd40a48d9af334090e4d9d73057ecc42f58
Lee Morgan <Lee.morgan@partner.samsung.com>
2014-12-08 17:22:44 +0000

    Emscripten minimal adaptor build
    A bare bones adaptor build that isn't compatible with toolkit.
    
    Change-Id: Ice7a5953d34da55d4121e3b4bceb212dbfac36e1

---
+++ platform-abstractions/emscripten/emscripten-callbacks.h
@@ -0,0 +1,79 @@
+#ifndef __DALI_EMSCRIPTEN_CALLBACKS_H__
+#define __DALI_EMSCRIPTEN_CALLBACKS_H__
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <dali/integration-api/platform-abstraction.h>
+#include <dali/integration-api/resource-cache.h>
+#include <dali/public-api/common/dali-common.h>
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Emscripten
+{
+
+Dali::Integration::BitmapPtr GetGlyphImage( const std::string& fontFamily, const std::string& fontStyle, float fontSize, uint32_t character );
+
+Dali::ImageDimensions LoadImageMetadata(const std::string filename,
+                                        Dali::ImageDimensions& size,
+                                        Dali::FittingMode::Type fittingMode,
+                                        Dali::SamplingMode::Type samplingMode,
+                                        bool orientationCorrection );
+
+Dali::Integration::BitmapPtr GetImage(const Dali::ImageDimensions& size,
+                                      const Dali::FittingMode::Type& scalingMode,
+                                      const Dali::SamplingMode::Type& samplingMode,
+                                      const bool orientationCorrection,
+                                      const std::string& filename );
+
+struct Statistics
+{
+  bool on;
+  float frameCount;
+
+  float lastFrameDeltaSeconds;
+  unsigned int lastSyncTimeMilliseconds;
+  unsigned int nextSyncTimeMilliseconds;
+
+  unsigned int keepUpdating; ///< A bitmask of KeepUpdating values
+  bool needsNotification;
+  float secondsFromLastFrame;
+
+Statistics() :on(true),
+    frameCount(0.0),
+    lastFrameDeltaSeconds(0.0),
+    lastSyncTimeMilliseconds(0.0),
+    nextSyncTimeMilliseconds(0.0),
+    keepUpdating(0),
+    needsNotification(false),
+    secondsFromLastFrame(0.0)
+  {};
+};
+
+extern Statistics stats;
+
+}; // namespace Emscripten
+}; // namespace Internal
+}; // namespace Dali
+
+#endif // header
---
+++ platform-abstractions/emscripten/handle-wrapper.h
@@ -0,0 +1,77 @@
+#ifndef __DALI_HANDLE_WRAPPER_H__
+#define __DALI_HANDLE_WRAPPER_H__
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <dali/public-api/dali-core.h>
+
+#include "emscripten/emscripten.h"
+#include "emscripten/bind.h"
+
+// INTERNAL INCLUDES
+#include "type-info-wrapper.h"
+
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Emscripten
+{
+
+bool HandleOk(Dali::Handle& self);
+
+void SetSelf(Dali::Handle& self, Dali::Handle& other);
+
+void SetProperty(Dali::Handle& self, const std::string& javascriptName, const Dali::Property::Value& value);
+
+Dali::Property::Value GetProperty(Dali::Handle& self, const std::string& javascriptName);
+
+int GetPropertyIndex(Dali::Handle& self, const std::string& javascriptName);
+
+std::vector<std::string> GetProperties(Dali::Handle& self);
+
+void SetProperty(Dali::Handle& self, const std::string& javascriptName, const Dali::Property::Value& value);
+
+Dali::Property::Type GetPropertyTypeFromName(Dali::Handle& self, const std::string& name);
+
+std::string GetPropertyTypeName(Dali::Handle& self, const std::string& name);
+
+Dali::Property::Index RegisterProperty(Dali::Handle& self, const std::string& name, const Dali::Property::Value& propertyValue);
+
+Dali::Property::Index RegisterAnimatedProperty(Dali::Handle& self, const std::string& name, const Dali::Property::Value& propertyValue);
+
+Dali::TypeInfo GetTypeInfo(Dali::Handle& self);
+
+// no mangling functions
+
+Dali::Property::Index RegisterPropertyNoMangling(Dali::Handle& self, const std::string& name, const Dali::Property::Value& propertyValue);
+
+std::string GetPropertyTypeNameNoMangling(Dali::Handle& self, const std::string& name);
+
+Dali::Property::Value GetPropertyNoMangling(Dali::Handle& self, const std::string& name);
+
+void SetPropertyNoMangling(Dali::Handle& self, const std::string& name, const Dali::Property::Value& value);
+
+
+}; // namespace Emscripten
+}; // namespace Internal
+}; // namespace Dali
+
+#endif // header
