78452e69a869ed4a0010985d7bf94c76e07e8bea
Lee Morgan <Lee.morgan@partner.samsung.com>
2014-12-08 17:22:44 +0000

    Emscripten minimal adaptor build
    A bare bones adaptor build that isn't compatible with toolkit.
    
    Change-Id: I0c4a8ab51d8455deb3bf6de44b59bc31a3048e43

---
+++ platform-abstractions/emscripten/actor-wrapper.cpp
@@ -0,0 +1,245 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "actor-wrapper.h"
+
+// EXTERNAL INCLUDES
+
+// INTERNAL INCLUDES
+#include "emscripten-utils.h"
+
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Emscripten
+{
+
+namespace
+{
+
+struct EmscriptenSignal
+{
+  EmscriptenSignal(const emscripten::val& callback, unsigned int id)
+    : mCallback(callback), mId(id) {};
+  emscripten::val mCallback;
+  unsigned int mId;
+  bool operator()()
+  {
+    Dali::Actor a = Dali::Stage::GetCurrent().GetRootLayer().FindChildById(mId);
+    if(a)
+    {
+      emscripten::val ret = mCallback( Dali::Internal::Emscripten::ActorWrapper(a) );
+    }
+    else
+    {
+      emscripten::val ret = mCallback();
+    }
+    return true;
+  }
+};
+
+}; // anon namespace
+
+
+//
+// ActorWrapper
+//
+ActorWrapper::ActorWrapper(Dali::Actor actor)
+  : HandleWrapper(actor), mActor(actor)
+{
+}
+
+
+ActorWrapper::~ActorWrapper()
+{
+}
+
+Dali::Actor ActorWrapper::GetActor() const
+{
+  return mActor;
+}
+
+void ActorWrapper::Add(ActorWrapper child)
+{
+  mActor.Add(child.mActor);
+}
+
+void ActorWrapper::Remove(ActorWrapper child)
+{
+  mActor.Remove(child.mActor);
+}
+
+ActorWrapper ActorWrapper::GetParent() const
+{
+  return ActorWrapper(mActor.GetParent());
+}
+
+ActorWrapper ActorWrapper::FindChildById(const unsigned int id)
+{
+  return ActorWrapper(mActor.FindChildById(id));
+}
+
+ActorWrapper ActorWrapper::FindChildByName(const std::string& actorAlias)
+{
+  // using alias which defaults to name
+  return ActorWrapper(mActor.FindChildByName(actorAlias));
+}
+
+ActorWrapper ActorWrapper::GetChildAt(unsigned int index) const
+{
+  return ActorWrapper(mActor.GetChildAt(index));
+}
+
+unsigned int ActorWrapper::GetChildCount() const
+{
+  return mActor.GetChildCount();
+}
+
+unsigned int ActorWrapper::GetId() const
+{
+  return mActor.GetId();
+}
+
+void ActorWrapper::SetImage(const std::string& data)
+{
+  Dali::ImageActor imageActor = Dali::ImageActor::DownCast(mActor);
+
+  if(imageActor)
+  {
+    imageActor.SetImage( GetImage( data ) );
+  }
+}
+
+void ActorWrapper::SetImageRawRGBA(unsigned int width, unsigned int height, const std::string& data)
+{
+  if( Dali::ImageActor imageActor = Dali::ImageActor::DownCast(mActor) )
+  {
+    imageActor.SetImage( CreateImageRGBA(width, height, data) );
+  }
+}
+
+void ActorWrapper::SetImageRawRGB(unsigned int width, unsigned int height, const std::string& data)
+{
+  if( Dali::ImageActor imageActor = Dali::ImageActor::DownCast(mActor) )
+  {
+    imageActor.SetImage( CreateImageRGB( width,height,data ) );
+  }
+}
+
+bool ActorWrapper::Ok()
+{
+  if(mActor)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void ActorWrapper::SetShaderEffect( ShaderEffectWrapper effect )
+{
+  if( Dali::RenderableActor renderable = Dali::RenderableActor::DownCast(mActor) )
+  {
+    renderable.SetShaderEffect( effect.GetShaderEffect() );
+  }
+}
+
+ShaderEffectWrapper ActorWrapper::GetShaderEffect() const
+{
+  if( Dali::RenderableActor renderable = Dali::RenderableActor::DownCast(mActor) )
+  {
+    return ShaderEffectWrapper( renderable.GetShaderEffect() );
+  }
+  else
+  {
+    return ShaderEffectWrapper();
+  }
+}
+
+void ActorWrapper::RemoveShaderEffect()
+{
+  if( Dali::RenderableActor renderable = Dali::RenderableActor::DownCast(mActor) )
+  {
+    renderable.RemoveShaderEffect();
+  }
+}
+
+bool ActorWrapper::ConnectSignal( const std::string& signalName,
+                                  const emscripten::val& javascriptFunction )
+{
+  return mHandle.ConnectSignal( this, signalName, EmscriptenSignal(javascriptFunction, mActor.GetId()) );
+}
+
+void ActorWrapper::SignalConnected( Dali::SlotObserver* slotObserver, Dali::CallbackBase* callback )
+{
+}
+
+void ActorWrapper::SignalDisconnected( Dali::SlotObserver* slotObserver, Dali::CallbackBase* callback )
+{
+}
+
+
+void ActorWrapper::Insert(int index, ActorWrapper child)
+{
+  if(Ok() && child.Ok())
+  {
+    mActor.Insert(index, child.GetActor());
+  }
+}
+
+unsigned int ActorWrapper::AddressOf()
+{
+  if(Ok())
+  {
+    return (unsigned int)&mActor.GetBaseObject();
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+std::vector<float> ActorWrapper::ScreenToLocal(float screenX, float screenY) const
+{
+  std::vector<float> ret;
+  float localX = 0.f;
+  float localY = 0.f;
+  bool ok = mActor.ScreenToLocal(localX, localY, screenX, screenY);
+  if( ok )
+  {
+    ret.push_back(localX);
+    ret.push_back(localY);
+    ret.push_back(1.0);
+  }
+  else
+  {
+    ret.push_back(0.0);
+    ret.push_back(0.0);
+    ret.push_back(0.0);
+  }
+  return ret;
+}
+
+
+
+}; // namespace Emscripten
+}; // namespace Internal
+}; // namespace Dali
---
+++ platform-abstractions/emscripten/material-wrapper.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "material-wrapper.h"
+
+// EXTERNAL INCLUDES
+
+// INTERNAL INCLUDES
+#include "emscripten-utils.h"
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Emscripten
+{
+
+MaterialWrapper::MaterialWrapper(const std::string name)
+{
+  mMaterial = Dali::Material::New(name);
+}
+
+void MaterialWrapper::SetOpacity(const float opacity)
+{
+  mMaterial.SetOpacity(opacity);
+}
+
+void MaterialWrapper::SetShininess(const float shininess)
+{
+  mMaterial.SetShininess(shininess);
+}
+
+void MaterialWrapper::SetAmbientColor(const Dali::Vector4& color)
+{
+  mMaterial.SetAmbientColor(color);
+}
+
+void MaterialWrapper::SetDiffuseColor(const Dali::Vector4& color)
+{
+  mMaterial.SetDiffuseColor(color);
+}
+
+void MaterialWrapper::SetEmissiveColor(const Dali::Vector4& color)
+{
+  mMaterial.SetEmissiveColor(color);
+}
+
+void MaterialWrapper::SetSpecularColor(const Dali::Vector4& color)
+{
+  mMaterial.SetSpecularColor(color);
+}
+
+
+void MaterialWrapper::SetDiffuseTextureRGBAData(unsigned int width, unsigned int height, const std::string& data)
+{
+  mMaterial.SetDiffuseTexture( CreateImageRGBA(width, height, data) );
+}
+
+void MaterialWrapper::SetDiffuseTextureRGBData(unsigned int width, unsigned int height, const std::string& data)
+{
+  mMaterial.SetDiffuseTexture( CreateImageRGB(width, height, data) );
+}
+
+void MaterialWrapper::SetOpacityTextureRGBAData(unsigned int width, unsigned int height, const std::string& data)
+{
+  mMaterial.SetOpacityTexture( CreateImageRGBA(width, height, data) );
+}
+
+void MaterialWrapper::SetOpacityTextureRGBData(unsigned int width, unsigned int height, const std::string& data)
+{
+  mMaterial.SetOpacityTexture( CreateImageRGB(width, height, data) );
+}
+
+
+}; // namespace Emscripten
+}; // namespace Internal
+}; // namespace Dali
