4b16e0d53a813a7c565b9bbc974db56374f58cb4
Paul Wisbey <p.wisbey@samsung.com>
2015-01-26 10:38:27 +0000

    Added some basic FontClient APIs
    Change-Id: I705a6a909c9a81d77342f12845485a34fe5ded1b

--- text-abstraction/public-api/font-client.h
+++ text-abstraction/public-api/font-client.h
@@ -17,60 +17,97 @@
  * limitations under the License.
  *
  */
+
 #include <dali/public-api/object/base-handle.h>
+#include "text-type-definitions.h"
+#include "glyph-metrics.h"
 
 namespace Dali
 {
 
+namespace TextAbstraction
+{
+
 namespace Internal DALI_INTERNAL
 {
-
-namespace TextAbstraction
-{
 class FontClient;
-} // TextAbstraction
-} // Internal
-
-namespace TextAbstraction
-{
-
+}
 
 /**
- *   FontClient API
+ * FontClient API
  *
+ * The font client caches information for each available font face, accessed using a FontId.
  */
 class DALI_IMPORT_API FontClient : public BaseHandle
 {
-
 public:
 
-    /**
-     * @brief Create an uninitialized TextAbstraction handle.
-     *
-     */
-    FontClient();
+  /**
+   * @brief Create an uninitialized TextAbstraction handle.
+   */
+  FontClient();
 
-    /**
-     * @brief Destructor
-     *
-     * This is non-virtual since derived Handle types must not contain data or virtual methods.
-     */
-    ~FontClient();
+  /**
+   * @brief Destructor
+   *
+   * This is non-virtual since derived Handle types must not contain data or virtual methods.
+   */
+  ~FontClient();
 
-    /**
-     * @brief This constructor is used by FontClient::Get().
-     *
-     * @param[in] fontClient  A pointer to the internal fontClient object.
-     */
-    explicit DALI_INTERNAL FontClient( Dali::Internal::TextAbstraction::FontClient* fontClient);
+  /**
+   * @brief Retrieve a handle to the FontClient instance.
+   *
+   * @return A handle to the FontClient
+   */
+  static FontClient Get();
 
-    /**
-     * @brief Retrieve a handle to the FontClient instance.
-     *
-     * @return A handle to the FontClient
-     */
-    static FontClient Get();
+  /**
+   * @brief Set the DPI of the target window.
+   *
+   * @note Multiple windows are not currently supported.
+   * @param[in] horizontalDpi The horizontal resolution in DPI.
+   * @param[in] verticalDpi The vertical resolution in DPI.
+   */
+  void SetDpi( unsigned int horizontalDpi, unsigned int verticalDpi );
 
+  /**
+   * @brief Retrieve the unique identifier for a font face.
+   *
+   * @param[in] path The path to a font file.
+   * @param[in] pointSize The point size in 26.6 fractional points; the default point size is 12.
+   * @param[in] face The index of the font face (optional).
+   * @return A valid font ID, or zero if the font does not exist.
+   */
+  FontId GetFontId( const std::string& path, PointSize26Dot6 pointSize = 12*64, FaceIndex faceIndex = 0 );
+
+  /**
+   * @brief Retrieve the metrics for a glyph.
+   *
+   * @param[in] fontId The ID of the font for this glyph.
+   * @param[in,out] metrics The metrics with an initialized glyph ID.
+   * On return, the remaining values will be initialized e.g. glyph size & bearing values.
+   * @return True if metrics were found.
+   */
+  bool CreateMetrics( FontId fontId, GlyphMetrics& metrics );
+
+  /**
+   * @brief Retrieve the metrics for a seris of glyphs.
+   *
+   * @param[in] fontId The ID of the font for these glyphs.
+   * @param[in,out] metrics An array of metrics with initialized glyph IDs.
+   * On return, the remaining values will be initialized e.g. glyph size & bearing values.
+   * @param[in] size The size of the array.
+   * @return True if metrics were found.
+   */
+  bool CreateMetrics( FontId fontId, GlyphMetrics* array, uint32_t size );
+
+public: // Not intended for application developers
+  /**
+   * @brief This constructor is used by FontClient::Get().
+   *
+   * @param[in] fontClient  A pointer to the internal fontClient object.
+   */
+  explicit DALI_INTERNAL FontClient( Internal::FontClient* fontClient );
 };
 
 } // namespace TextAbstraction
