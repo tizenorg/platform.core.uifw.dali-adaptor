2f694fa68b8158649bfa9be9759b7e8f0cb14c8f
Jonghyun Ho <jonghyun.ho@samsung.com>
2015-02-11 19:12:57 +0900

    Separating integration-devel from devel package for 2nd party to use adaptor
     - hidden for 3rd party
    
    Change-Id: I10d32ad6eff736088b9e05ba2b061915e465e0a8

--- adaptors/wayland/ecore-wl-render-surface.h
+++ adaptors/wayland/ecore-wl-render-surface.h
@@ -21,24 +21,17 @@
 // EXTERNAL INCLUDES
 #include <string>
 #include <boost/thread.hpp>
-#include <Ecore.h>
 #include <Ecore_Wayland.h>
 #include <dali/public-api/common/dali-common.h>
 
 // INTERNAL INCLUDES
-#include <render-surface-impl.h>
+#include <render-surface.h>
 #include <ecore-wl-types.h>
-#include <gl/egl-implementation.h>
 
 namespace Dali
 {
 
-namespace Internal
-{
-
-namespace Adaptor
-{
-class TriggerEvent;
+class TriggerEventInterface;
 
 namespace ECore
 {
@@ -48,31 +41,27 @@ namespace ECore
  * @todo change namespace to ECore_Wayland as the class
  * is no longer pure Wayland.
  */
-class RenderSurface : public Internal::Adaptor::RenderSurface
+class DALI_IMPORT_API EcoreWlRenderSurface : public Dali::RenderSurface
 {
 public:
   /**
     * Uses an Wayland surface to render to.
-    * @param [in] type the type of surface passed in
     * @param [in] positionSize the position and size of the surface
     * @param [in] surface can be a X-window or X-pixmap (type must be unsigned int).
-    * @param [in] display connection to X-server if the surface is a X window or pixmap (type must be void * to X display struct)
     * @param [in] name optional name of surface passed in
     * @param [in] isTransparent if it is true, surface has 32 bit color depth, otherwise, 24 bit
     */
-  RenderSurface( SurfaceType type,
-                 Dali::PositionSize positionSize,
-                 Any surface,
-                 Any display,
-                 const std::string& name,
-                 bool isTransparent = false);
+  EcoreWlRenderSurface(Dali::PositionSize positionSize,
+                       Any surface,
+                       const std::string& name,
+                       bool isTransparent = false);
 
   /**
    * Destructor.
    * Will delete the display, if it has ownership.
    * Will delete the window/pixmap if it has owner ship
    */
-  virtual ~RenderSurface();
+  virtual ~EcoreWlRenderSurface();
 
 protected:
   /**
@@ -84,112 +73,75 @@ protected:
 public: // API
 
   /**
+   * @brief Get window handle
+   *
    * @return the Ecore X window handle
    */
   Ecore_Wl_Window* GetWlWindow();
 
   /**
-   * @return the Main X display
-   */
-  WlDisplay* GetMainDisplay();
-
-  /**
-   * Sets the render notification trigger to call when render thread is completed a frame
-   * @param renderNotification to use
-   */
-  void SetRenderNotification( TriggerEvent* renderNotification );
-
-  /**
-   * Get the surface as an Ecore_X_drawable
+   * Get the surface as an Ecore_Wl_Window
    */
   virtual Ecore_Wl_Window* GetDrawable();
 
 public: // from Dali::RenderSurface
 
   /**
-   * @copydoc Dali::RenderSurface::GetType()
-   */
-  virtual Dali::RenderSurface::SurfaceType GetType() = 0;
-
-  /**
-   * @copydoc Dali::RenderSurface::GetSurface()
-   */
-  virtual Any GetSurface() = 0;
-
-  /**
-   * @copydoc Dali::RenderSurface::GetDisplay()
-   */
-  virtual Any GetDisplay();
-
-  /**
    * @copydoc Dali::RenderSurface::GetPositionSize()
    */
   virtual PositionSize GetPositionSize() const;
 
-public:  // from Internal::Adaptor::RenderSurface
+  /**
+   * @copydoc Dali::RenderSurface::InitializeEgl()
+   */
+  virtual void InitializeEgl( EglInterface& egl ) = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::CreateEglSurface()
+   * @copydoc Dali::RenderSurface::CreateEglSurface()
    */
   virtual void CreateEglSurface( EglInterface& egl ) = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::DestroyEglSurface()
+   * @copydoc Dali::RenderSurface::DestroyEglSurface()
    */
   virtual void DestroyEglSurface( EglInterface& egl ) = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::ReplaceEGLSurface()
+   * @copydoc Dali::RenderSurface::ReplaceEGLSurface()
    */
   virtual bool ReplaceEGLSurface( EglInterface& egl ) = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::MoveResize()
+   * @copydoc Dali::RenderSurface::MoveResize()
    */
   virtual void MoveResize( Dali::PositionSize positionSize);
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::GetDpi()
+   * @copydoc Dali::RenderSurface::SetViewMode()
    */
-  virtual void GetDpi( unsigned int& dpiHorizontal, unsigned int& dpiVertical ) const;
+  void SetViewMode( ViewMode viewMode );
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::Map()
+   * @copydoc Dali::RenderSurface::StartRender()
    */
-  virtual void Map();
+  virtual void StartRender() = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::TransferDisplayOwner()
-   */
-  virtual void TransferDisplayOwner( Internal::Adaptor::RenderSurface& newSurface );
-
-  /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::ConsumeEvents()
-   */
-  virtual void ConsumeEvents();
-
-  /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::SetViewMode()
-   */
-  void SetViewMode( ViewMode );
-
-  /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::PreRender()
+   * @copydoc Dali::RenderSurface::PreRender()
    */
   virtual bool PreRender( EglInterface& egl, Integration::GlAbstraction& glAbstraction ) = 0;
 
   /**
-   * @copydoc Dali::Internal::Adaptor::RenderSurface::PostRender()
+   * @copydoc Dali::RenderSurface::PostRender()
    */
   virtual void PostRender( EglInterface& egl, Integration::GlAbstraction& glAbstraction, unsigned int timeDelta, bool replacingSurface ) = 0;
 
-private:
-
   /**
-   * Sets the display, if display parameter is empty it opens a new display
-   * @param display
+   * @copydoc Dali::RenderSurface::ReleaseLock()
    */
-  void SetDisplay( Any display );
+  virtual void ReleaseLock() = 0;
+
+private:
 
   /**
    * Get the surface id if the surface parameter is not empty
@@ -213,22 +165,14 @@ protected:
 
 protected: // Data
 
-  WlDisplay*                  mMainDisplay;        ///< Wayland-connection for rendering
-  SurfaceType                 mType;               ///< type of renderable
   PositionSize                mPosition;           ///< Position
   std::string                 mTitle;              ///< Title of window which shows from "xinfo -topvwins" command
   ColorDepth                  mColorDepth;         ///< Color depth of surface (32 bit or 24 bit)
-  TriggerEvent*               mRenderNotification; ///< Render notification trigger
 
   bool                        mOwnSurface;         ///< Whether we own the surface (responsible for deleting it)
-  bool                        mOwnDisplay;         ///< Whether we own the display (responsible for deleting it)
 };
 
 } // namespace ECore
-
-} // namespace Adaptor
-
-} // namespace internal
 
 } // namespace Dali
 
