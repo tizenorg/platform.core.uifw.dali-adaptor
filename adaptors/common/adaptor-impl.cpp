ba11aa17f50b1f4fcc612bbdb122cc54e8b7bdb9
Heeyong Song <heeyong.song@samsung.com>
2015-03-10 13:33:23 +0900

    Make a full screen window in the mobile emulator which has an HD screen
    Change-Id: I364501a6306dd525ec48e983b18ef801b74a35c0

--- adaptors/common/adaptor-impl.cpp
+++ adaptors/common/adaptor-impl.cpp
@@ -366,17 +366,19 @@ void Adaptor::Start()
 
   // NOTE: dpi must be set before starting the render thread
   // use default or command line settings if not run on device
-#ifdef __arm__
-  // set the DPI value for font rendering
-  unsigned int dpiHor, dpiVer;
-  dpiHor = dpiVer = 0;
-  mSurface->GetDpi(dpiHor, dpiVer);
+  if( mHDpi == 0 || mVDpi == 0 )
+  {
+    unsigned int dpiHor, dpiVer;
+    dpiHor = dpiVer = 0;
+    mSurface->GetDpi(dpiHor, dpiVer);
 
-  // tell core about the value
-  mCore->SetDpi(dpiHor, dpiVer);
-#else
-  mCore->SetDpi(mHDpi, mVDpi);
-#endif
+    // tell core about the value
+    mCore->SetDpi(dpiHor, dpiVer);
+  }
+  else
+  {
+    mCore->SetDpi(mHDpi, mVDpi);
+  }
 
   // Tell the core the size of the surface just before we start the render-thread
   PositionSize size = mSurface->GetPositionSize();
