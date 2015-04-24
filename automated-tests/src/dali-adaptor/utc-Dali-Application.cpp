a99a226a32080b6b0ddc4569af064eb19bf8ac07
suhyung Eom <suhyung.eom@samsung.com>
2015-04-24 15:07:47 +0900

    Add Public API UTC for DaliApplication
    Signed-off-by: suhyung Eom <suhyung.eom@samsung.com>
    Change-Id: Ia1bcefc525f0b8e96563c87cb4c5d954609f5c63

--- automated-tests/src/dali-adaptor/utc-Dali-Application.cpp
+++ automated-tests/src/dali-adaptor/utc-Dali-Application.cpp
@@ -18,7 +18,20 @@
 #include <dali/dali.h>
 #include <dali-test-suite-utils.h>
 
+extern int gArgc;
+extern char ** gArgv;
+
 using namespace Dali;
+
+const float DEFAULT_STEREO_BASE( 65.0f );
+
+
+enum TEST_TYPE
+{
+  NEW = 0,
+  VIEW_MODE = 1,
+  STEREO_BASE = 2
+};
 
 void utc_dali_application_startup(void)
 {
@@ -36,8 +49,15 @@ namespace
 struct MyTestApp : public ConnectionTracker
 {
   MyTestApp( Application& app)
+    :MyTestApp(app, NEW)
+  {
+  }
+
+  MyTestApp( Application& app, int type )
   : initCalled( false ),
-    application( app )
+    tickCalled( false ),
+    application( app ),
+    mTestType( type )
   {
     application.InitSignal().Connect( this, &MyTestApp::Create );
   }
@@ -45,19 +65,71 @@ struct MyTestApp : public ConnectionTracker
   void Create(Application& app)
   {
     initCalled = true;
+
+    timer = Timer::New(500);
+    timer.TickSignal().Connect(this, &MyTestApp::Tick);
+    timer.Start();
+
+    excuteTest();
   }
 
-  void Quit()
+  bool Tick()
   {
+    tickCalled = true;
+    timer.Stop();
     application.Quit();
+    return true;
+  }
+
+  bool IsDoneSuccessfully()
+  {
+    return (initCalled && tickCalled);
+  }
+
+  void excuteTest()
+  {
+    switch (mTestType)
+    {
+      case VIEW_MODE:
+        testViewMode();
+        break;
+      case STEREO_BASE:
+        testStereoBase();
+        break;
+    }
+  }
+
+  void testViewMode()
+  {
+    application.SetViewMode(STEREO_HORIZONTAL);
+    DALI_TEST_CHECK( application.GetViewMode() ==  STEREO_HORIZONTAL );
+
+    application.SetViewMode(STEREO_VERTICAL);
+    DALI_TEST_CHECK( application.GetViewMode() ==  STEREO_VERTICAL );
+
+    application.SetViewMode(STEREO_INTERLACED);
+    DALI_TEST_CHECK( application.GetViewMode() ==  STEREO_INTERLACED );
+  }
+
+  void testStereoBase()
+  {
+    application.SetStereoBase(DEFAULT_STEREO_BASE * 2);
+    DALI_TEST_CHECK( application.GetStereoBase() ==  DEFAULT_STEREO_BASE * 2 );
+
+    application.SetStereoBase(DEFAULT_STEREO_BASE);
+    DALI_TEST_CHECK( application.GetStereoBase() ==  DEFAULT_STEREO_BASE);
   }
 
   // Data
   bool initCalled;
+  bool tickCalled;
   Application& application;
+  int mTestType;
+  Timer timer;
 };
 
 } // unnamed namespace
+
 int UtcDaliApplicationNew(void)
 {
   Application application = Application::New();
@@ -65,6 +137,45 @@ int UtcDaliApplicationNew(void)
   MyTestApp testApp( application );
 
   DALI_TEST_CHECK( application );
+
+  END_TEST;
+}
+
+int UtcDaliApplicationInitSignal(void)
+{
+  Application application = Application::New(&gArgc, &gArgv);
+
+  MyTestApp testApp( application );
+
+  application.MainLoop();
+
+  DALI_TEST_CHECK( testApp.IsDoneSuccessfully() );
+
+  END_TEST;
+}
+
+int UtcDaliApplicationViewModeSetAndGet(void)
+{
+  Application application = Application::New(&gArgc, &gArgv);
+
+  MyTestApp testApp( application, VIEW_MODE );
+
+  application.MainLoop();
+
+  DALI_TEST_CHECK( testApp.IsDoneSuccessfully() );
+
+  END_TEST;
+}
+
+int UtcDaliApplicationStereoBaseSetAndGet(void)
+{
+  Application application = Application::New(&gArgc, &gArgv);
+
+  MyTestApp testApp( application, STEREO_BASE );
+
+  application.MainLoop();
+
+  DALI_TEST_CHECK( testApp.IsDoneSuccessfully() );
 
   END_TEST;
 }
