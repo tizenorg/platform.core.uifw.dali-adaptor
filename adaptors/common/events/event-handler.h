6c9787ad8a28f8ef81efcc685d7a82e7c9ad749c
Heeyong Song <heeyong.song@samsung.com>
2015-05-22 13:37:08 +0900

    Change WheelEvent   - Replace MouseWheelEvent with WheelEvent   - Add custom wheel event type
    Change-Id: Ie56513a13b2b6d512e151223d1c47623b4b055b8

--- adaptors/common/events/event-handler.h
+++ adaptors/common/events/event-handler.h
@@ -79,10 +79,10 @@ public:
   void FeedTouchPoint( TouchPoint& point, int timeStamp );
 
   /**
-   * Feed (Send) mouse wheel event to core and gesture manager
-   * @param[in]  wheelEvent The mouse wheel event
+   * Feed (Send) wheel event to core and gesture manager
+   * @param[in]  wheelEvent The wheel event
    */
-  void FeedWheelEvent( MouseWheelEvent& wheelEvent );
+  void FeedWheelEvent( WheelEvent& wheelEvent );
 
   /**
    * Feed (Send) key event to core
@@ -129,10 +129,10 @@ private:
   void SendEvent(KeyEvent& keyEvent);
 
   /**
-   * Send mouse wheel event to core.
-   * @param[in]  wheelEvent The mouse wheel event
+   * Send wheel event to core.
+   * @param[in]  wheelEvent The wheel event
    */
-  void SendMouseWheelEvent( MouseWheelEvent& wheelEvent );
+  void SendWheelEvent( WheelEvent& wheelEvent );
 
   /**
    * Send a style change event to the style monitor.
