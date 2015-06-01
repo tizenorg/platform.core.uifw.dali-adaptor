a04806de57592de2419464456b748d3644e09eb3
Adeel Kazmi <adeel.kazmi@samsung.com>
2015-06-01 15:20:43 +0100

    Update copyright headers
    Change-Id: I6a6231796e78caff00077fa1e0af1e6c682a16a6

--- adaptors/common/gl/egl-image-extensions.h
+++ adaptors/common/gl/egl-image-extensions.h
@@ -2,7 +2,7 @@
 #define __DALI_INTERNAL_EGL_IMAGE_EXTENSIONS_H__
 
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- platform-abstractions/portable/file-closer.h
+++ platform-abstractions/portable/file-closer.h
@@ -1,7 +1,7 @@
 #ifndef _DALI_INTERNAL_PLATFORM_FILECLOSER_H__
 #define _DALI_INTERNAL_PLATFORM_FILECLOSER_H__
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
