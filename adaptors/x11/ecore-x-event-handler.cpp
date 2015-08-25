7dd6ff309d7a08f30d7d6e8fc74da8ba1b7bfb3e
taeyoon <taeyoon0.lee@samsung.com>
2015-08-25 16:12:14 +0900

    Fix prevent issues
     - Fix xiEvent.mask, xiEvent.mask_len
     - oss = ""+keepUpdateStatus+"":"" to oss = ""; oss+= (keepUpdatingStatus?"":"");
    
    Change-Id: Iba01850958c432e73da5b49fbaf3fc9dddcc4ea8

--- adaptors/x11/ecore-x-event-handler.cpp
+++ adaptors/x11/ecore-x-event-handler.cpp
@@ -372,10 +372,13 @@ struct EventHandler::Impl
             mXiDeviceId = xiEventMask.deviceid;
 
             // SelectXi2Event
-            xiEventMask.mask = (unsigned char*)(calloc( 1, XIMaskLen( XI_LASTEVENT ) ) );
+            Dali::Vector< unsigned char > mask;
+            mask.Reserve( XIMaskLen( XI_LASTEVENT ) );
+            xiEventMask.mask = mask.Begin();
+
             XISetMask( xiEventMask.mask, XI_RawMotion );
 
-            xiEventMask.mask_len = sizeof( xiEventMask.mask );
+            xiEventMask.mask_len = XIMaskLen( XI_LASTEVENT) * sizeof( unsigned char );
 
             int ret = XISelectEvents( display, rootWindow, &xiEventMask, 1 );
             if( ret == 0 )
@@ -387,8 +390,6 @@ struct EventHandler::Impl
             {
               DALI_LOG_INFO( gImfLogging, Debug::General, "Failed to Select Events\n" );
             }
-
-            free( xiEventMask.mask );
           }
 
           if( deviceInfo != NULL )
