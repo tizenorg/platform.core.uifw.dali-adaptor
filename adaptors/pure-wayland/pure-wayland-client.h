eb22cdcae535559bd3e5643e884e13bd8faea9a1
Nick Holland <nick.holland@partner.samsung.com>
2015-09-02 16:06:35 +0100

    [3.0] Prototype pure wayland adaptor for DALi
    [based on the patchset 10 of https://review.tizen.org/gerrit/#/c/47361/]
    
    Supports:
    Wayland Shell,
    XDG Sheel,
    Input: Keyboard with XKB keyboard mappings/ pointer / touch input
    
    Change-Id: Ia875425027da8b2658b998c8010d8d18d34d1cc5

---
+++ adaptors/pure-wayland/pure-wayland-client.h
@@ -0,0 +1,129 @@
+#ifndef __DALI_PURE_WAYLAND_CLIENT_H__
+#define __DALI_PURE_WAYLAND_CLIENT_H__
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <wl-types.h>
+
+// INTERNAL INCLUDES
+#include <file-descriptor-monitor.h>
+#include "pure-wayland-input.h"
+
+
+// on target make sure adaptor.so is search first
+// export LD_PRELOAD=/usr/lib/libdali-adaptor.so
+DALI_IMPORT_API int wl_display_flush(struct wl_display *display);
+
+// dispatch performs a flush then a blocking read
+DALI_IMPORT_API int wl_display_dispatch(struct wl_display *display);
+
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+class WindowEventInterface;
+class PerformanceInterface;
+
+/**
+ * Client used to talk to Wayland server over a UNIX domain stream socket.
+ *
+ * Brief overview of Wayland:
+ *
+ * Transport mechanism = Socket
+ * Display = handles all the data sent from and to the compositor
+ * Display has a file descriptor that can be monitored for read / write events
+ *
+ * wl client function calls will place messages in a queue
+ * Calling wl_display_flush() will flush the messages to  the server
+ *
+ * Incoming data is handled in two steps: queueing and dispatching.
+ * In the queue step, the data coming from the display fd is interpreted and
+ * added to a queue. On the dispatch step, the handler for the incoming event is called.
+ *
+ * default queue is dispatched by calling wl_display_dispatch().
+ *
+ * The compositor sends out the frame event every time it draws a frame.
+ * wl_display_frame_callback() to schedule a callback per frame.
+ *
+ *
+ * wl_display_dispatch(). This will dispatch any events queued on the default queue and
+ * attempt to read from the display fd if it's empty.
+ * Events read are then queued on the appropriate queues according to the proxy assignment.
+ *
+ *
+ */
+class WaylandClient
+{
+
+public:
+
+  /**
+   * @brief Constructor
+   */
+  WaylandClient();
+
+  /**
+   * @brief Destructor
+   */
+  ~WaylandClient();
+
+  /**
+   * @brief Connect to Wayland server and setup internal data structures
+   */
+  void Initialise();
+
+  /**
+   *
+   */
+  void AssignWindowEventInterface( WindowEventInterface* eventInterface);
+
+  void SetPerformanceInterface( PerformanceInterface* performanceInterface );
+
+
+public: // change to private
+
+  /**
+   * @brief
+   */
+  void InstallFileDescriptorMonitor();
+
+  void FileDescriptorCallback( FileDescriptorMonitor::EventType eventTypeMask );
+
+  void GetWaylandInterfaces();
+
+  WaylandInput  mInputManager;
+  WlDisplay* mDisplay;        ///< Wayland display, handles all the data sent from and to the compositor
+  WlShell* mShell;            ///< shell
+  WlCompositor* mCompositor;  ///< compositor
+  WlXdgShell* mXdgShell;
+  int mDisplayFileDescriptor;
+  FileDescriptorMonitor* mFileDescriptorMonitor;
+
+};
+} // Internal
+} // Adaptor
+} // Dali
+
+#endif
--- adaptors/wayland/egl-implementation-wl.cpp
+++ adaptors/wayland/egl-implementation-wl.cpp
@@ -21,12 +21,13 @@
 #include <gl/egl-implementation.h>
 
 // EXTERNAL INCLUDES
+#include <stdio.h>
 #include <dali/integration-api/debug.h>
 #include <dali/public-api/common/dali-common.h>
 #include <dali/public-api/common/dali-vector.h>
 
 // INTERNAL INCLUDES
-#include <ecore-wl-render-surface.h>
+//#include <ecore-wl-render-surface.h>
 
 namespace Dali
 {
@@ -74,9 +75,17 @@ bool EglImplementation::InitializeGles( EGLNativeDisplayType display, bool isOwn
   {
     mEglNativeDisplay = display;
 
+
     //@todo see if we can just EGL_DEFAULT_DISPLAY instead
     mEglDisplay = eglGetDisplay(mEglNativeDisplay);
+    if(  mEglDisplay == EGL_NO_DISPLAY)
+    {
+      DALI_ASSERT_ALWAYS( "eglGetDisplay failed EGL_NO_DISPLAY" &&  0);
+    }
     EGLint error = eglGetError();
+
+    //printf("EglImplementation::InitializeGles native display = %p, EGL display = %p\n", mEglNativeDisplay, mEglDisplay);
+
 
     if( mEglDisplay == NULL && error != EGL_SUCCESS )
     {
@@ -85,9 +94,28 @@ bool EglImplementation::InitializeGles( EGLNativeDisplayType display, bool isOwn
 
     EGLint majorVersion = 0;
     EGLint minorVersion = 0;
-    if ( !eglInitialize( mEglDisplay, &majorVersion, &minorVersion ) )
+    EGLBoolean ret = eglInitialize( mEglDisplay, &majorVersion, &minorVersion );
+    switch (ret)
     {
-      return false;
+    case EGL_FALSE:
+    {
+      DALI_ASSERT_ALWAYS( "eglInitialize failed EGL_FALSE" &&  0);
+      break;
+    }
+    case EGL_BAD_DISPLAY:
+    {
+      DALI_ASSERT_ALWAYS( "eglInitialize failed EGL_BAD_DISPLAY " &&  0);
+      break;
+    }
+    case EGL_NOT_INITIALIZED:
+    {
+      DALI_ASSERT_ALWAYS( "eglInitialize failed EGL_NOT_INITIALIZED " &&  0);
+      break;
+    }
+    default:
+    {
+      break;
+    }
     }
     eglBindAPI(EGL_OPENGL_ES_API);
 
@@ -237,6 +265,8 @@ void EglImplementation::MakeContextCurrent()
       eglQueryString(mEglDisplay, EGL_VERSION),
       eglQueryString(mEglDisplay, EGL_CLIENT_APIS),
       eglQueryString(mEglDisplay, EGL_EXTENSIONS));
+
+  eglSwapInterval( mEglDisplay, 1 );
 }
 
 void EglImplementation::MakeContextNull()
@@ -367,6 +397,8 @@ void EglImplementation::ChooseConfig( bool isWindowType, ColorDepth depth )
   configAttribs.PushBack( 1 );
   configAttribs.PushBack( EGL_NONE );
 
+//  printf("eglChooseConfig  mEglDisplay = %p\n ",mEglDisplay);
+
   if ( eglChooseConfig( mEglDisplay, &(configAttribs[0]), &mEglConfig, 1, &numConfigs ) != EGL_TRUE )
   {
     EGLint error = eglGetError();
@@ -418,6 +450,7 @@ void EglImplementation::CreateSurfaceWindow( EGLNativeWindowType window, ColorDe
 
   // egl choose config
   ChooseConfig(mIsWindow, mColorDepth);
+//  printf("EglImplementation::CreateSurfaceWindow  display = %p\n",mEglDisplay);
 
   mEglSurface = eglCreateWindowSurface( mEglDisplay, mEglConfig, mEglNativeWindow, NULL );
   TEST_EGL_ERROR("eglCreateWindowSurface");
