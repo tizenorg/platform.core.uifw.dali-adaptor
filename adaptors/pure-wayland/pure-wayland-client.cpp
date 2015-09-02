ba33b67ad875b102e20158ba88f6becc8daa8898
Nick Holland <nick.holland@partner.samsung.com>
2015-09-02 16:06:35 +0100

    Prototype pure wayland adaptor for DALi
    Change-Id: I6bc01be2d369d242c06248660561949db47f5433

---
+++ adaptors/pure-wayland/pure-wayland-client.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+// CLASS HEADER
+#include "pure-wayland-client.h"
+
+// EXTERNAL INCLUDES
+#include <stdio.h>
+#include <cstring>
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+namespace
+{
+void RegistryGlobalCallback( void *data,
+           struct wl_registry *wlRegistry,
+           uint32_t name,
+           const char *interface,
+           uint32_t version)
+{
+
+  WaylandClient* client = static_cast<   WaylandClient* >( data );
+
+  printf("Wayland registry callback with %s \n",interface);
+
+  if (strcmp( interface, wl_compositor_interface.name) == 0)
+  {
+    client->mCompositor = static_cast<Dali::WlCompositor*>(wl_registry_bind( wlRegistry, name , &wl_compositor_interface, version ));
+  }
+  else if (strcmp(interface, wl_shell_interface.name) == 0)
+  {
+    client->mShell = static_cast<Dali::WlShell*>(wl_registry_bind( wlRegistry, name , &wl_shell_interface, version));
+  }
+}
+
+
+void RegistryGlobalCallbackRemove(void *data, struct wl_registry *registry, uint32_t id)
+{
+  // occurs when a device is unplugged
+}
+
+const struct wl_registry_listener RegistryListener =
+{
+   RegistryGlobalCallback,
+   RegistryGlobalCallbackRemove,
+};
+
+}
+WaylandClient::WaylandClient()
+{
+}
+WaylandClient::~WaylandClient()
+{
+  wl_shell_destroy( mShell );
+
+  wl_compositor_destroy( mCompositor );
+
+  wl_display_disconnect( mDisplay );
+}
+
+void WaylandClient::Initialise()
+{
+  // Connect to a Wayland socket on the Wayland server/
+  // NULL = use default display aka "wayland-0"
+  // returns a new display context object
+
+  mDisplay =  wl_display_connect( NULL );
+
+  DALI_ASSERT_ALWAYS( mDisplay && "wl_display_connect failed");
+
+  // Monitor the display file descriptor used to communicate with Wayland server
+  InstallFileDescriptorMonitor();
+
+  // Get the interfaces to compositor / shell etc
+  GetWaylandInterfaces();
+
+}
+
+void WaylandClient::GetWaylandInterfaces()
+{
+  // get and listen to the registry
+  WlRegistry* registry = wl_display_get_registry( mDisplay );
+
+  wl_registry_add_listener(  registry, &RegistryListener, this);
+
+  // send and receive, to trigger the registry listener callback
+  wl_display_roundtrip( mDisplay );
+
+  wl_registry_destroy( registry );
+}
+
+void WaylandClient::InstallFileDescriptorMonitor()
+{
+  // get the file descriptor
+  mDisplayFileDescriptor = wl_display_get_fd( mDisplay );
+
+  // create the callback that gets triggered when a read / write event occurs
+  CallbackBase* callback =  MakeCallback( this, &WaylandClient::FileDescriptorCallback);
+
+  // monitor read and write events
+  int events = FileDescriptorMonitor::FD_READABLE | FileDescriptorMonitor::FD_WRITABLE;
+
+  mFileDescriptorMonitor = new FileDescriptorMonitor( mDisplayFileDescriptor, callback, events );
+
+}
+void WaylandClient::FileDescriptorCallback( FileDescriptorMonitor::EventType eventTypeMask )
+{
+  int x =0;
+  x++;
+  if( (x % 500 )== 0)
+  {
+    printf(" wayland FileDescriptorCallback\n");
+  }
+  if( eventTypeMask & FileDescriptorMonitor::FD_READABLE )
+  {
+    // read and dispatch
+    wl_display_dispatch( mDisplay );
+  }
+  else
+  {
+    // not sure if this is correct
+    wl_display_flush( mDisplay );
+  }
+}
+}
+}
+}
---
+++ adaptors/pure-wayland/pure-wl-render-surface.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include "wl-render-surface.h"
+#include <stdio.h>
+// EXTERNAL INCLUDES
+#include <dali/integration-api/gl-abstraction.h>
+#include <dali/integration-api/debug.h>
+#include <cstring>
+#include <uv.h>
+#include <unistd.h>
+
+// INTERNAL INCLUDES
+#include <wl-types.h>
+#include "pure-wayland-client.h"
+#include <trigger-event.h>
+
+namespace Dali
+{
+
+#if defined(DEBUG_ENABLED)
+Debug::Filter* gRenderSurfaceLogFilter = Debug::Filter::New(Debug::Verbose, false, "LOG_WAYLAND_RENDER_SURFACE");
+#endif
+
+namespace Wayland
+{
+
+namespace
+{
+
+void ShellSurfacePing(void *data, struct wl_shell_surface *shell_surface, uint32_t serial)
+{
+  printf(" ShellSurfacePing \n");
+  wl_shell_surface_pong(shell_surface, serial);
+}
+
+void ShellSurfaceConfigure(void *data, struct wl_shell_surface *shell_surface, uint32_t edges, int32_t width, int32_t height)
+{
+  printf(" ShellSurfaceConfigure %d, %d \n", width, height );
+}
+
+const struct wl_shell_surface_listener ShellSurfaceListener =
+{
+    ShellSurfacePing,
+    ShellSurfaceConfigure,
+};
+
+}
+WlRenderSurface::WlRenderSurface(Dali::PositionSize positionSize,
+                                           Any surface,
+                                           const std::string& name,
+                                           bool isTransparent)
+: mPosition(positionSize),
+  mTitle(name),
+  mRenderNotification(NULL),
+  mColorDepth(isTransparent ? COLOR_DEPTH_32 : COLOR_DEPTH_24),
+  mOwnSurface(false)
+{
+
+}
+
+void WlRenderSurface::Init( Any surface )
+{
+  printf(" WlRenderSurface::Init \n");
+  //@todo support surface being passed in
+  mOwnSurface = true;
+
+  mWaylandClient = new  Dali::Internal::Adaptor::WaylandClient;
+
+  mWaylandClient->Initialise();
+}
+
+void WlRenderSurface::CreateSurface()
+{
+  printf("WlRenderSurface::CreateSurface \n");
+
+  // Create the surface
+  // A Wayland surface is a rectangular area that is displayed on the screen.
+  // It has a location, size and pixel contents.
+  mWindow.mSurface = wl_compositor_create_surface( mWaylandClient->mCompositor );
+
+  DALI_ASSERT_ALWAYS(  mWindow.mSurface && "wl_compositor_create_surface failed" );
+
+  // keep track of the surface id
+  mWindow.mSurfaceId = wl_proxy_get_id( reinterpret_cast<struct wl_proxy *>(mWindow.mSurface ));
+
+  // A surface in Wayland needs to be assigned a role.
+  // This gives the surface the role of a shell surface.
+  // It allows the surface to be treated like a toplevel, fullscreen or popup window.
+  // which can be moved, resize or maximized and have associate metadata like title and class.
+  mWindow.mShellSurface = wl_shell_get_shell_surface( mWaylandClient->mShell, mWindow.mSurface);
+
+  DALI_ASSERT_ALWAYS(mWindow.mShellSurface && "wl_shell_get_shell_surface failed" );
+
+  if( mTitle.length() )
+  {
+    wl_shell_surface_set_title(  mWindow.mShellSurface, mTitle.c_str() );
+  }
+
+   wl_shell_surface_set_toplevel( mWindow.mShellSurface);
+ //  wl_shell_surface_set_fullscreen( mWindow.mShellSurface,   WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
+   //   0, NULL);
+
+  wl_shell_surface_add_listener( mWindow.mShellSurface , &ShellSurfaceListener, 0);
+
+  wl_display_roundtrip( mWaylandClient->mDisplay );
+}
+
+WlRenderSurface::~WlRenderSurface()
+{
+
+  delete mWaylandClient;
+
+}
+
+void WlRenderSurface::SetRenderNotification(TriggerEventInterface* renderNotification)
+{
+  mRenderNotification = renderNotification;
+}
+
+WlWindow* WlRenderSurface::GetWlWindow()
+{
+  return &mWindow;
+}
+
+WlWindow* WlRenderSurface::GetDrawable()
+{
+  return  &mWindow;
+}
+
+PositionSize WlRenderSurface::GetPositionSize() const
+{
+  return mPosition;
+}
+
+void WlRenderSurface::MoveResize( Dali::PositionSize positionSize )
+{
+
+}
+
+void WlRenderSurface::SetViewMode( ViewMode viewMode )
+{
+}
+
+unsigned int WlRenderSurface::GetSurfaceId( Any surface ) const
+{
+  // not used
+  return 0;
+}
+
+} // namespace Wayland
+
+} // namespace Dali
