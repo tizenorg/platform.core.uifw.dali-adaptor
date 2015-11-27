a70832427241b37d2805708b088c49166540f304
Paul Wisbey <p.wisbey@samsung.com>
2015-11-27 15:28:55 +0000

    Temp
    Change-Id: I3f975d5f9bf712e67676418ab509ca41972afabb

--- text/dali/internal/text-abstraction/font-client-impl.cpp
+++ text/dali/internal/text-abstraction/font-client-impl.cpp
@@ -203,11 +203,11 @@ GlyphIndex FontClient::GetGlyphIndex( FontId fontId, Character charcode )
   return mPlugin->GetGlyphIndex( fontId, charcode );
 }
 
-bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, bool horizontal, int maxFixedSize )
+bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, GlyphType type, bool horizontal, int maxFixedSize )
 {
   CreatePlugin();
 
-  return mPlugin->GetGlyphMetrics( array, size, horizontal, maxFixedSize );
+  return mPlugin->GetGlyphMetrics( array, size, type, horizontal, maxFixedSize );
 }
 
 BufferImage FontClient::CreateBitmap( FontId fontId, GlyphIndex glyphIndex )
@@ -215,6 +215,13 @@ BufferImage FontClient::CreateBitmap( FontId fontId, GlyphIndex glyphIndex )
   CreatePlugin();
 
   return mPlugin->CreateBitmap( fontId, glyphIndex );
+}
+
+void FontClient::CreateGlyphyBlob( FontId fontId, GlyphIndex glyphIndex, GlyphyBlob*& blob, unsigned int& blobLength )
+{
+  CreatePlugin();
+
+  return mPlugin->CreateGlyphyBlob( fontId, glyphIndex, blob, blobLength );
 }
 
 const GlyphInfo& FontClient::GetEllipsisGlyph( PointSize26Dot6 pointSize )
