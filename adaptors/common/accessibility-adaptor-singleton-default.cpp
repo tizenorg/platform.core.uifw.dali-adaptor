ca5f38ca65295d9a1e481b031716ecce9d9fb984
Paul Wisbey <p.wisbey@samsung.com>
2015-12-16 11:00:14 +0000

    Temp
    Change-Id: I370b26763867beca7b8064a1b3e9b6a243f8a098

--- adaptors/common/accessibility-adaptor-singleton-default.cpp
+++ adaptors/common/accessibility-adaptor-singleton-default.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,14 +19,10 @@
 #include "accessibility-adaptor-impl.h"
 
 // EXTERNAL INCLUDES
-#include <vconf.h>
-
 #include <dali/public-api/object/type-registry.h>
-#include <dali/integration-api/debug.h>
 
 // INTERNAL INCLUDES
 #include <singleton-service-impl.h>
-#include <system-settings.h>
 
 namespace Dali
 {
@@ -37,45 +33,8 @@ namespace Internal
 namespace Adaptor
 {
 
-namespace
+namespace // unnamed namespace
 {
-
-const char * DALI_VCONFKEY_SETAPPL_ACCESSIBILITY_DBUS_TTS = "db/setting/accessibility/atspi";
-
-bool GetEnabledVConf()
-{
-  int isEnabled = 0;
-  vconf_get_bool( DALI_VCONFKEY_SETAPPL_ACCESSIBILITY_DBUS_TTS, &isEnabled );
-
-  if( isEnabled == 0 )
-  {
-    vconf_get_bool( VCONFKEY_SETAPPL_ACCESSIBILITY_TTS, &isEnabled );
-  }
-
-  return (bool)isEnabled;
-}
-
-#if defined(DEBUG_ENABLED)
-Debug::Filter* gAccessibilityAdaptorLogFilter = Debug::Filter::New( Debug::NoLogging, false, "LOG_ACCESSIBILITY_ADAPTOR" );
-#endif
-
-void AccessibilityOnOffNotification(keynode_t* node, void* data)
-{
-  AccessibilityAdaptor* adaptor = static_cast<AccessibilityAdaptor*>( data );
-
-  bool isEnabled = GetEnabledVConf();
-
-  DALI_LOG_INFO( gAccessibilityAdaptorLogFilter, Debug::General, "[%s:%d] %s\n", __FUNCTION__, __LINE__, isEnabled ? "ENABLED" : "DISABLED" );
-
-  if( isEnabled )
-  {
-    adaptor->EnableAccessibility();
-  }
-  else
-  {
-    adaptor->DisableAccessibility();
-  }
-}
 
 BaseHandle Create()
 {
@@ -87,19 +46,6 @@ BaseHandle Create()
     if ( service )
     {
       Dali::AccessibilityAdaptor adaptor = Dali::AccessibilityAdaptor( new AccessibilityAdaptor() );
-      Adaptor& adaptorImpl = AccessibilityAdaptor::GetImplementation( adaptor );
-
-      bool isEnabled = GetEnabledVConf();
-
-      if( isEnabled )
-      {
-        adaptorImpl.EnableAccessibility();
-      }
-      DALI_LOG_INFO( gAccessibilityAdaptorLogFilter, Debug::General, "[%s:%d] %s\n", __FUNCTION__, __LINE__, isEnabled ? "ENABLED" : "DISABLED" );
-
-      vconf_notify_key_changed( DALI_VCONFKEY_SETAPPL_ACCESSIBILITY_DBUS_TTS, AccessibilityOnOffNotification, &adaptorImpl );
-      vconf_notify_key_changed( VCONFKEY_SETAPPL_ACCESSIBILITY_TTS, AccessibilityOnOffNotification, &adaptorImpl );
-
       service.Register( typeid( adaptor ), adaptor );
       handle = adaptor;
     }
@@ -133,8 +79,7 @@ Dali::AccessibilityAdaptor AccessibilityAdaptor::Get()
 
 void AccessibilityAdaptor::OnDestroy()
 {
-  vconf_ignore_key_changed( VCONFKEY_SETAPPL_ACCESSIBILITY_TTS, AccessibilityOnOffNotification );
-  vconf_ignore_key_changed( DALI_VCONFKEY_SETAPPL_ACCESSIBILITY_DBUS_TTS, AccessibilityOnOffNotification );
+  // Nothing to do here
 }
 
 } // namespace Adaptor
