7ab5310f5fb8f25a3349400d475bde3f9718c371
suhyung Eom <suhyung.eom@samsung.com>
2016-04-28 16:28:44 +0900

    Implement watchApp class
    Signed-off-by: suhyung Eom <suhyung.eom@samsung.com>
    Change-Id: I056931e477e8f5becfc20efd79380d3d223769ec

--- adaptors/common/framework.h
+++ adaptors/common/framework.h
@@ -42,6 +42,11 @@ namespace Adaptor
 class Framework
 {
 public:
+  enum ApplicationType
+  {
+    NORMAL,       ///<  normal appFramework
+    WATCH     ///< watch appFramework
+  };
 
   /**
    * Observer class for the framework.
@@ -82,6 +87,21 @@ public:
     virtual void OnAppControl(void *) {}
 
     /**
+     * Invoked at every second
+     */
+    virtual void OnTimeTick(void *) {}
+
+    /**
+     * Invoked at every second in ambient mode
+     */
+    virtual void OnAmbientTick(void *) {}
+
+    /**
+     * Invoked when the device enters or exits ambient mode
+     */
+    virtual void OnAmbientChanged(bool ambient) {}
+
+    /**
      * Invoked when the language of the device is changed.
      */
     virtual void OnLanguageChanged() {}
@@ -104,13 +124,14 @@ public:
 
 public:
 
-  /**
+    /**
    * Constructor
    * @param[in]  observer  The observer of the Framework.
    * @param[in]  argc      A pointer to the number of arguments.
    * @param[in]  argv      A pointer the the argument list.
+   * @param[in]  type      A pointer the the argument list.
    */
-  Framework( Observer& observer, int* argc, char ***argv );
+  Framework( Observer& observer, int* argc, char ***argv, ApplicationType type = NORMAL );
 
   /**
    * Destructor
@@ -160,6 +181,12 @@ private:
   Framework& operator=(Framework&);
 
 private:
+
+  /**
+   * Called when the application is created.
+   */
+  bool Create();
+
   /**
    * Called by the App framework when an application lifecycle event occurs.
    * @param[in] type The type of event occurred.
@@ -201,7 +228,6 @@ private: // impl members
 
   struct Impl;
   Impl* mImpl;
-
 };
 
 } // namespace Adaptor
---
+++ adaptors/common/watch-impl.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include "watch-impl.h"
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+WatchPtr Watch::New(
+  int* argc,
+  char **argv[],
+  const std::string& stylesheet,
+  Dali::Watch::WINDOW_MODE windowMode)
+{
+  WatchPtr watch ( new Watch (argc, argv, stylesheet, windowMode ) );
+  return watch;
+}
+
+Watch::Watch( int* argc, char** argv[], const std::string& stylesheet, Dali::Watch::WINDOW_MODE windowMode )
+: Application(argc, argv, stylesheet, windowMode, Framework::WATCH)
+{
+}
+
+Watch::~Watch()
+{
+}
+
+void Watch::OnTimeTick(void *time)
+{
+  Dali::Watch watch(this);
+  mTickSignal.Emit( watch, (WatchTime *)time );
+}
+
+void Watch::OnAmbientTick(void *time)
+{
+  Dali::Watch watch(this);
+  mAmbientTickSignal.Emit( watch, (WatchTime *)time );
+}
+
+void Watch::OnAmbientChanged(bool ambient)
+{
+  Dali::Watch watch(this);
+  mAmbientChangeSignal.Emit( watch, ambient );
+}
+
+} // namespace Adaptor
+
+} // namespace Internal
+
+} // namespace Dali
