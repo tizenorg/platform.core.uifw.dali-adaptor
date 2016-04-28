652592266bf4dd771becb6f17ec6f540a12165b4
suhyung Eom <suhyung.eom@samsung.com>
2016-04-28 16:28:44 +0900

    Implement watchApp class
    Signed-off-by: suhyung Eom <suhyung.eom@samsung.com>
    Change-Id: I056931e477e8f5becfc20efd79380d3d223769ec

---
+++ adaptors/common/watch-impl.h
@@ -0,0 +1,126 @@
+#ifndef __DALI_INTERNAL_WATCH_H__
+#define __DALI_INTERNAL_WATCH_H__
+
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+#include <watch.h>
+#include <application-impl.h>
+
+namespace Dali
+{
+class Adaptor;
+//class Window;
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+class WatchApplication;
+typedef IntrusivePtr<WatchApplication> WatchApplicationPtr;
+
+/**
+ * Implementation of the WatchApplication class.
+ */
+class WatchApplication : public Application
+//, public Framework::Observer
+{
+public:
+  typedef Dali::WatchApplication::WatchTime WatchTime;
+  typedef Dali::WatchApplication::WatchTimeSignal WatchTimeSignal;
+  typedef Dali::WatchApplication::WatchBoolSignal WatchBoolSignal;
+  /**
+   * Create a new watch
+   * @param[in]  argc        A pointer to the number of arguments
+   * @param[in]  argv        A pointer to the argument list
+   * @param[in]  stylesheet  The path to user defined theme file
+   * @param[in]  windowMode  A member of Dali::Watch::WINDOW_MODE
+   */
+  static WatchApplicationPtr New( int* argc, char **argv[], const std::string& stylesheet, WINDOW_MODE windowMode );
+
+public:
+  /**
+   * Called every second
+   */
+  virtual void OnTimeTick(void *time);
+
+  /**
+   * Called every second in ambient mode
+   */
+  virtual void OnAmbientTick(void *time);
+
+  /**
+   * Called when the device enters or exits ambient mode
+   */
+  virtual void OnAmbientChanged(bool ambient);
+
+public:
+
+  // Signals
+  WatchTimeSignal                        mTickSignal;
+  WatchTimeSignal                        mAmbientTickSignal;
+  WatchBoolSignal                      mAmbientChangeSignal;
+
+  /**
+   * Private Constructor
+   * @param[in]  argc        A pointer to the number of arguments
+   * @param[in]  argv        A pointer to the argument list
+   * @param[in]  stylesheet  The path to user defined theme file
+   * @param[in]  windowMode  A member of Dali::Watch::WINDOW_MODE
+   */
+  WatchApplication( int* argc, char **argv[], const std::string& stylesheet, WINDOW_MODE windowMode );
+
+  /**
+   * Destructor
+   */
+  virtual ~WatchApplication();
+
+  // Undefined
+  WatchApplication(const WatchApplication&);
+  WatchApplication& operator=(WatchApplication&);
+
+};
+
+inline WatchApplication& GetImplementation(Dali::WatchApplication& watch)
+{
+  DALI_ASSERT_ALWAYS(watch && "watch handle is empty");
+
+  BaseObject& handle = watch.GetBaseObject();
+
+  return static_cast<Internal::Adaptor::WatchApplication&>(handle);
+}
+
+inline const WatchApplication& GetImplementation(const Dali::WatchApplication& watch)
+{
+  DALI_ASSERT_ALWAYS(watch && "Timre handle is empty");
+
+  const BaseObject& handle = watch.GetBaseObject();
+
+  return static_cast<const Internal::Adaptor::WatchApplication&>(handle);
+}
+
+
+} // namespace Adaptor
+
+} // namespace Internal
+
+} // namespace Dali
+
+#endif // __DALI_INTERNAL_WATCH_H__
